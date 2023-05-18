2ee0cbd4a13a25d7867c9f66f30f315289c47383
siiika <mamo2020mamo75@gmail.com>
2023-05-19 02:09:54 +0300

    lectuer tasks

---
+++ tasks/DrAmr/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using  Decision_Making_Metods.UnderCertainly;
+namespace DrAmr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           MiniMax_Regret c = new  MiniMax_Regret(3, 3);
+            c.data = new int[,] {
+            {4075,5000,6100 },
+            {0,5250,12080 },
+            { 2500,7000,10375}
+            };
+           // c.getRegretTable();
+            c.Print();
+            Console.WriteLine("=============");
+            string ans;
+            int[] arr = c.Solve(out ans);
+            Console.WriteLine("==================================");
+            Console.WriteLine(ans);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ tasks/WinFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Decision_Making_Metods.UnderCertainly;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int rows;
+        int columns;
+        public Form1()
+        {
+            InitializeComponent();
+             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rows = Convert.ToInt32(textBox2.Text);
+            columns = Convert.ToInt32(textBox3.Text);
+            Certainly c=null;
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    c = new Maximax(rows,columns);
+                    break;
+                case 1:
+                    c = new Maximin(rows, columns);
+                    break;
+                case 2:
+                    c = new MiniMax_Regret(rows, columns);
+                    break;
+                case 3:
+                    c = new EquallyLikelyCriterion(rows, columns);
+                    break;
+                default:
+                    label5.Text = "Please Select Correct Method";
+                    break;
+            }
+            string msg=null;
+            int[] answer= c?.Solve(out msg);
+            textBox1.Text +=$"{msg}\n";
+            foreach (var item in answer)
+            {
+                textBox1.Text += $"{item}\n";
+            }
+
+
+        }
+        //private int[,] SetValues(string val)
+        //{
+        //    string[] values = val.Split(',');
+        //    int[,] arr = new int[rows, columns];
+        //    for (int i = 0; i < rows; i++)
+        //    {
+        //        for (int j = 0; j < columns; j++)
+        //        {
+        //            arr[i, j] = Convert.ToInt32(values[i]);
+        //        }
+        //    }
+
+        //}
+    }
+}
